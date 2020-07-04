9cc55f1649258cade13bf45dbbc851fe6cbef78c
fengs <quxuanfeng@gmail.com>
2020-07-04 15:26:51 +0800

    2020/07/04 15:21 根据教程重新编写了堆排序的代码
    原先的代码问题出在调整堆的时候只调整了当前层，忽略了当前调整可能会使之前调整完的子大顶堆失效

--- Aalgorithm/src/main/java/com/algorithm/HeapSort.java
+++ Aalgorithm/src/main/java/com/algorithm/HeapSort.java
@@ -1,29 +1,50 @@
 package com.algorithm;
 
 import com.algorithm.utils.GetRandomArray;
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Arrays;
-import java.util.Random;
 
 public class HeapSort {
     public static void main(String[] args) {
-        /*Random random = new Random();
-        int[] input = new int[10];
-        for (int i = 1; i < 10; i++) {
-            input[i] = random.nextInt(999);
-        }*/
-        int[] input = GetRandomArray.getArray(100000);
-        int[] answer = input;
-        Arrays.sort(answer);//答案
+        int[] input = GetRandomArray.getArray(20);
+        heapSort(input);
 
-        int[] result = Sort.heapSort(input);//堆排序（大顶堆，由大至小）
-
-        System.out.println(Arrays.equals(input, answer));
+        System.out.println(Arrays.toString(input));
     }
+
+    public static void heapSort (int[] input){
+        int temp;
+        for (int i = input.length/2 -1; i >= 0; i--) {
+            adjustHeap(input, i, input.length);
+        }
+        for (int j = input.length-1; j>0; j--){
+            temp = input[j];
+            input[j] = input[0];
+            input[0] = temp;
+
+            adjustHeap(input, 0, j);
+        }
+
+    }
+    public static void adjustHeap(int[] arr, int i, int length){
+        int temp =arr[i];
+        for (int j = i*2+1; j < length ; j = j*2+1) {
+            if (j+1<length){
+                if (arr[j] < arr[j+1])
+                    j++;
+            }
+            if (arr[j] > temp){
+                arr[i] = arr[j];
+                i = j;
+                arr[i] = temp;
+            }else
+                break;
+        }
+    }
+
 }
 
-class Sort {
+
+/*class Sort {
     static int current;
     static int temp;//用于临时存储交换中的变量
 
@@ -46,28 +67,30 @@ class Sort {
     static int[] makeHeapUpToDown(@NotNull int[] input, int size){
         int i = 1;
         while(i <= (size>>1)){
-            try {
-                if (input[i << 1] > input[i] || input[(i << 1) + 1]  > input[i]){
-                    if(input[i << 1] > input[(i << 1) + 1]){//左子节点大于右子节点
-                        temp = input[i];//保存父节点的值
-                        input[i] = input[i << 1];//将左子节点的值赋给父节点
-                        input[i << 1] = temp;//将父节点的值赋给左子节点
-                    }else {                                                                         //右子节点大于左子节点
-                        temp = input[i];//保存父节点的值
-                        input[i] = input[(i << 1) + 1];//将右子节点的值赋给父节点
-                        input[(i << 1) + 1] = temp;//将父节点的值赋给右子节点
-                    }
+            if (input[i << 1] > input[i] || input[(i << 1) + 1]  > input[i]){
+                if(input[i << 1] > input[(i << 1) + 1]){//左子节点大于右子节点
+                    temp = input[i];//保存父节点的值
+                    input[i] = input[i << 1];//将左子节点的值赋给父节点
+                    input[i << 1] = temp;//将父节点的值赋给左子节点
+                }else {                                                                         //右子节点大于左子节点
+                    temp = input[i];//保存父节点的值
+                    input[i] = input[(i << 1) + 1];//将右子节点的值赋给父节点
+                    input[(i << 1) + 1] = temp;//将父节点的值赋给右子节点
                 }
-                i++;
-            } catch (ArrayIndexOutOfBoundsException ignored) {
-
             }
+            i++;
         }
+       *//* if ((i << 1) == size){//这里用于处理只有一个子节点的父节点
+            temp = input[i];//保存父节点的值
+            input[i] = input[size];//将左子节点的值赋给父节点
+            input[size] = temp;//将父节点的值赋给左子节点
+            current--;
+        }*//*
         return input;
     }
     static int[] makeHeapDownToUp(@NotNull int[] input){
         current = (input.length - 1) >> 1;
-        if ((current << 1) == input.length){//这里用于处理只有一个子节点的父节点
+        if (((current << 1) == input.length-1) && input[input.length-1] > input[current]){//这里用于处理只有一个子节点的父节点
             temp = input[current];//保存父节点的值
             input[current] = input[current << 1];//将左子节点的值赋给父节点
             input[current << 1] = temp;//将父节点的值赋给左子节点
@@ -93,4 +116,4 @@ class Sort {
         }
         return input;
     }
-}
+}*/
--- Aalgorithm/src/main/java/com/algorithm/QuickSort.java
+++ Aalgorithm/src/main/java/com/algorithm/QuickSort.java
@@ -6,17 +6,50 @@ import org.jetbrains.annotations.NotNull;
 public class QuickSort {
     public static void main(String[] args) {
         @NotNull int[] input = GetRandomArray.getArray(10);
-        input = quickSort.doSort(input);
+        quickSort(input, 1, input.length-1);
+
+        for (int i :
+                input) {
+            System.out.print(i+" ");
+        }
+
     }
+
+    public static void quickSort(int[] arr,int low,int high){
+        int i,j,temp,t;
+        if(low>high){
+            return;
+        }
+        i=low;
+        j=high;
+        //temp就是基准位
+        temp = arr[low];
+
+        while (i<j) {
+            //先看右边，依次往左递减
+            while (temp<=arr[j]&&i<j) {
+                j--;
+            }
+            //再看左边，依次往右递增
+            while (temp>=arr[i]&&i<j) {
+                i++;
+            }
+            //如果满足条件则交换
+            if (i<j) {
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+
+        }
+        //最后将基准为与i和j相等位置的数字交换
+        arr[low] = arr[i];
+        arr[i] = temp;
+        //递归调用左半数组
+        quickSort(arr, low, j-1);
+        //递归调用右半数组
+        quickSort(arr, j+1, high);
+    }
+
 }
 
-class quickSort{
-    public static int[] doSort(int[] input) {
-        int split;
-        while (true){
-            split = (int)(Math.random()*(input.length))+1;
-            System.out.println(split);
-        }
-        /*return null;*/
-    }
-}
